9e269dd0e14f84f851efb0b48feb7fe320b9dfc6
Yanning Liu <1320140523@qq.com>
2026-02-24 16:33:03 +0800

    加入light状态2

--- src/main/java/frc/robot/subsystems/light/Light.java
+++ src/main/java/frc/robot/subsystems/light/Light.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.light;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,11 +9,23 @@ import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
 
-import edu.wpi.first.wpilibj.DriverStation;
-
 public class Light extends SubsystemBase{
     private final CANdle candle = new CANdle(LightConstants.candleCanID);
-    
+    //状态枚举
+    public enum LightState{
+        ALLIANCE, //显示联盟颜色
+        IDLE, //空载颜色
+        INTAKE, //吸球状态
+        SHOOTING, //射球状态
+        AIM, //瞄准状态
+        ERROR, //错误状态
+
+    }
+
+    private volatile LightState state = LightState.ALLIANCE;
+    private LightState lastAppliedState = null;
+    private DriverStation.Alliance lastAppliedAlliance = null;
+
     public Light(){
         candle.getConfigurator().apply(LightConstants.getLightConfig());
         for (int i = 0; i < LightConstants.kCandleLightNum; ++i) {
@@ -41,4 +54,59 @@ public class Light extends SubsystemBase{
             setRightBarColor(c);
         }, this);
     }
+
+    public void setState(LightState state){
+        this.state = state;
+        this.lastAppliedState = null; 
+    }
+
+    public Command setStateCommand(LightState state){
+        return Commands.runOnce(() -> setState(state),this);
+    }
+    
+    @Override
+    public void periodic(){
+        DriverStation.Alliance alliance = DriverStation.getAlliance().get();
+        if (state == lastAppliedState && (state != LightState.ALLIANCE || alliance == lastAppliedAlliance)) {
+            return;
+        }
+        RGBWColor apply;
+        switch (state) {
+            case ALLIANCE:
+                if (alliance == DriverStation.Alliance.Red) {
+                    apply = LightConstants.kRed;
+                } else if (alliance == DriverStation.Alliance.Blue) {
+                    apply = LightConstants.kBlue;
+                } else {
+                    apply = LightConstants.onboardColor;
+                }
+                break;
+            case INTAKE:
+                apply = LightConstants.kGreen;
+                break;
+            case SHOOTING:
+                apply = LightConstants.kYellow;
+                break;
+            case AIM:
+                apply = LightConstants.kCyan;
+                break;
+            case ERROR:
+                candle.setControl(
+                new StrobeAnimation(8, 67)
+                    .withColor(LightConstants.kRed)
+                );
+                lastAppliedState = state;
+                lastAppliedAlliance = alliance;
+                return;
+            case IDLE:
+            default:
+                apply = LightConstants.defaultColor;
+                break;
+        }
+        lastAppliedState = state;
+        lastAppliedAlliance = alliance;
+        setLeftBarColor(apply);
+        setRightBarColor(apply);
+    }
+
 }
