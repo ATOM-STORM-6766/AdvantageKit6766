0abc8ea85d02063d88d9e475e62e8098e38ed499
Sail-wang <85862570+Sail-wang@users.noreply.github.com>
2026-03-15 10:38:17 +0800

    Update at 'Sun Mar 15 10:38:17 CST 2026'

--- src/main/java/frc/robot/subsystems/hood/Hood.java
+++ src/main/java/frc/robot/subsystems/hood/Hood.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.hood;
 
 import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
