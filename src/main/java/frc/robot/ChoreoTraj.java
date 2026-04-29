135424863f94ac385fe2c8b4749cadec67c22fdd
Dongxue Qin <z5342771@ad.unsw.edu.au>
2026-04-30 00:12:13 +1000

    降低了飞坡前速度，让飞坡前速度都为0

--- src/main/java/frc/robot/ChoreoTraj.java
+++ src/main/java/frc/robot/ChoreoTraj.java
@@ -146,21 +146,21 @@ public record ChoreoTraj(
     public static final ChoreoTraj L2 = new ChoreoTraj(
         "L2",
         OptionalInt.empty(),
-        7.44453,
+        7.68722,
         new Pose2d(3, 5.6, Rotation2d.fromRadians(2.37295)),
         new Pose2d(3, 5.6, Rotation2d.fromRadians(1.5708))
     );
     public static final ChoreoTraj L2$0 = new ChoreoTraj(
         "L2",
         OptionalInt.of(0),
-        6.42174,
+        6.51353,
         new Pose2d(3, 5.6, Rotation2d.fromRadians(2.37295)),
         new Pose2d(5.8, 5.6, Rotation2d.fromRadians(1.5708))
     );
     public static final ChoreoTraj L2$1 = new ChoreoTraj(
         "L2",
         OptionalInt.of(1),
-        1.0227900000000005,
+        1.1736899999999997,
         new Pose2d(5.8, 5.6, Rotation2d.fromRadians(1.5708)),
         new Pose2d(3, 5.6, Rotation2d.fromRadians(1.5708))
     );
